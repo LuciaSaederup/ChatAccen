8bd3479d22c0690aa7d305eb5078852bae38028f
lucia.saederup <luciasaederup@gmail.com>
2023-05-19 12:00:47 -0300

    Finalizacion microservicio billing

--- ChatAccen/Billing/src/main/java/chat/accen/Controller/BillingController.java
+++ ChatAccen/Billing/src/main/java/chat/accen/Controller/BillingController.java
@@ -1,10 +1,13 @@
 package chat.accen.Controller;
 
+import chat.accen.Service.BillingService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/billing")
 public class BillingController {
+
+    private BillingService billingService;
 
 }
--- ChatAccen/Billing/src/main/java/chat/accen/Service/BillingService.java
+++ ChatAccen/Billing/src/main/java/chat/accen/Service/BillingService.java
@@ -1,7 +1,12 @@
 package chat.accen.Service;
 
 import org.springframework.stereotype.Service;
-
+import reactor.core.publisher.Mono;
+import chat.accen.Model.Billing;
 @Service
 public interface BillingService {
+    public Mono<Billing> getQuestion(long id);
+
+    public Mono<Billing> addQuestion();
+
 }
