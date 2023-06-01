641645e6da9dc40570d92b1550522159e5b2a86b
javi <javier.macoc@accenture.com>
2023-06-01 15:22:18 -0300

    modif en roles

--- ChatAccen/User/src/main/java/chat/accen/Model/Role.java
+++ ChatAccen/User/src/main/java/chat/accen/Model/Role.java
@@ -3,39 +3,30 @@ package chat.accen.Model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Table;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
 public class Role implements GrantedAuthority {
 
-	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
 	@ManyToMany(mappedBy = "roles")
 	private Set<User> users;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
+	
 	@Override
 	public String getAuthority() {
 		return email;
