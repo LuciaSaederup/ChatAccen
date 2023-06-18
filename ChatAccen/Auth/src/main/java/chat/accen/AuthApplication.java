4c15f4340a703cd69908dcea3994ab713ab1a0f7
javi <javier.macoc@accenture.com>
2023-06-18 13:53:51 -0300

    Inicio de config eureka

--- ChatAccen/Auth/src/main/java/chat/accen/AuthApplication.java
+++ ChatAccen/Auth/src/main/java/chat/accen/AuthApplication.java
@@ -2,8 +2,10 @@ package chat.accen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
--- ChatAccen/eureka-server/src/main/java/chat/accen/EurekaServer.java
+++ ChatAccen/eureka-server/src/main/java/chat/accen/EurekaServer.java
@@ -2,14 +2,14 @@ package chat.accen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class AnswersApplication {
+@EnableEurekaServer
+public class EurekaServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnswersApplication.class, args);
+		SpringApplication.run(EurekaServer.class, args);
 	}
 
 }
